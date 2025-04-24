a150db4970b6ceda4a9555f578a99f0138b3df5a
21Lexar <the-charon@hotmail.com>
2025-04-24 19:18:06 +0500

    Add Check Functions

--- src/Database.cpp
+++ src/Database.cpp
@@ -14,30 +14,74 @@ path GetPath(){
 }
 
 void CreateFile(){
-  path currentDir = current_path();
-  path outputPath = currentDir.parent_path() / "data";
-  create_directories(outputPath);
-  path filePath = outputPath / "users.txt";
+  path filePath = GetPath();
   ofstream file(filePath);
   file.close();
 }
 
-void StoreEmail(string email){
+void StoreEmail(const string email){
 
   path filePath = GetPath();
   if (filesystem::is_empty(filePath)) {
      ofstream file(filePath, ios::out);
-     file << email;
+     file << email <<endl;
      file.close();
   } else {
      ofstream file(filePath, ios::app);
-     file << email;
+     file << email << endl;
      file.close();
   }
 }
 
+void StorePassword(const string password){
+  path filePath = GetPath();
+  ofstream file(filePath, ios::app);
+  file << password <<endl;
+  file.close();
+}
+
+bool CheckEmail(const string email){
+  path filePath = GetPath();
+  ifstream file(filePath);
+  string line;
+  while (getline(file, line)){
+    if (email == line){
+      file.close();
+      return true;
+    }
+  }
+  file.close();
+  return false; 
+}
+bool CheckCredentials(const string email ,const string password){
+  path filePath = GetPath();
+  ifstream file(filePath);
+  string line;
+  bool found = false;
+  while(getline(file, line)){
+    if (found){
+      if (password == line){
+        file.close();
+        return true;
+      }
+    }
+    if (email == line){
+      found = true;
+    }
+  }
+  file.close();
+  return false;
+}
 
 int main(){
-  CreateFile();
-  StoreEmail("abc@example.com");
+  if (CheckEmail("gigglesafari@gmail.com")){
+    cout << "Email Found";
+  }else {
+    cout << "Email Not FOund";
+  }
+  if (CheckCredentials("gigglesafari@gmail.com", "jaysonbloody")){
+    cout << "Cred found";
+  }else {
+    cout << "Cred not found";
+  }
 }
