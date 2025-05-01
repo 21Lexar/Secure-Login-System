24c8b26fdf7f9b24a4a0ea424fbd14207b43a110
21Lexar <the-charon@hotmail.com>
2025-05-01 08:35:01 +0500

    Update Build Script

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-g++ src/main.cpp src/LoginManager.cpp src/Encryption.cpp src/Database.cpp src/OTPGenerator.cpp -Iinclude -o SecureLoginSystem
+
+# Set the output binary name
+OUTPUT="SecureLoginSystem"
+
+# Set the source files
+SOURCES="src/main.cpp src/LoginManager.cpp src/Encryption.cpp src/Database.cpp src/OTPGenerator.cpp src/SendMail.cpp"
+
+# Set the include directory
+INCLUDE_DIR="-Iinclude"
+
+# Set the required libraries
+LIBRARIES="-lcurl"
+
+# Compile the project
+echo "Compiling the project..."
+g++ $SOURCES $INCLUDE_DIR -o $OUTPUT $LIBRARIES
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful! Executable created: $OUTPUT"
+else
+    echo "Compilation failed!"
+fi
