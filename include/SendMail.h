b86a8f152737f2dfec82fc973791c588fb87f232
21Lexar <the-charon@hotmail.com>
2025-05-13 14:40:24 +0500

    Add Creds Image

--- include/SendMail.h
+++ include/SendMail.h
@@ -3,7 +3,6 @@
 
 #include <string>
 
-// Function to send an email with the provided receiver email and OTP
 void sendEmail(const std::string& receiver_email, const int& otp);
 
-#endif // SENDMAIL_H
+#endif 
